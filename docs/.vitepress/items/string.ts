693e333b64b2ca23213e0017a3ea1b1a06d0f20c
haoziqaq <357229046@qq.com>
2024-11-06 00:28:50 +0800

    docs: update docs

--- docs/.vitepress/items/string.ts
+++ docs/.vitepress/items/string.ts
@@ -8,4 +8,6 @@ export const stringItems = [
   { text: 'upperFirst', link: '/string/upper-first' },
   { text: 'lowerFirst', link: '/string/lower-first' },
   { text: 'slash', link: '/string/slash' },
+  { text: 'ensurePrefix', link: '/string/ensure-prefix' },
+  { text: 'ensureSuffix', link: '/string/ensure-suffix' },
 ]
