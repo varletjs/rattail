29da4d0a1fa79c6b0d7db226d981f6ea1f246e33
haoziqaq <357229046@qq.com>
2024-12-08 03:15:15 +0800

    types: improve maxBy minBy types

--- src/math/maxBy.ts
+++ src/math/maxBy.ts
@@ -1,7 +1,3 @@
 export function maxBy<T>(arr: T[], fn: (val: T) => number) {
-  if (!arr.length) {
-    return
-  }
-
   return arr.reduce((result, item) => (fn(result) > fn(item) ? result : item), arr[0])
 }
--- src/math/minBy.ts
+++ src/math/minBy.ts
@@ -1,7 +1,3 @@
 export function minBy<T>(arr: T[], fn: (val: T) => number) {
-  if (!arr.length) {
-    return
-  }
-
   return arr.reduce((result, item) => (fn(result) < fn(item) ? result : item), arr[0])
 }
