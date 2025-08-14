a76100ce124242e184886395836f920ec2f5c853
haoziqaq <357229046@qq.com>
2025-08-14 19:49:28 +0800

    types: improve types

--- src/util/duration.ts
+++ src/util/duration.ts
@@ -12,47 +12,47 @@ export function duration() {
     valueOf,
   }
 
-  function years(value: number) {
+  function years(value: number): typeof ctx {
     ctx.value += value * 365 * 24 * 60 * 60 * 1000
     return ctx
   }
 
-  function months(value: number) {
+  function months(value: number): typeof ctx {
     ctx.value += value * 30 * 24 * 60 * 60 * 1000
     return ctx
   }
 
-  function weeks(value: number) {
+  function weeks(value: number): typeof ctx {
     ctx.value += value * 7 * 24 * 60 * 60 * 1000
     return ctx
   }
 
-  function days(value: number) {
+  function days(value: number): typeof ctx {
     ctx.value += value * 24 * 60 * 60 * 1000
     return ctx
   }
 
-  function hours(value: number) {
+  function hours(value: number): typeof ctx {
     ctx.value += value * 60 * 60 * 1000
     return ctx
   }
 
-  function minutes(value: number) {
+  function minutes(value: number): typeof ctx {
     ctx.value += value * 60 * 1000
     return ctx
   }
 
-  function seconds(value: number) {
+  function seconds(value: number): typeof ctx {
     ctx.value += value * 1000
     return ctx
   }
 
-  function milliseconds(value: number) {
+  function milliseconds(value: number): typeof ctx {
     ctx.value += value
     return ctx
   }
 
-  function valueOf(options: { milliseconds: boolean } = { milliseconds: true }) {
+  function valueOf(options: { milliseconds: boolean } = { milliseconds: true }): number {
     return options.milliseconds ? ctx.value : ctx.value / 1000
   }
 
