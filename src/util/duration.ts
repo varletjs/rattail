2a09a6c11170ba31fc99eda2459f242560f7b7bc
haoziqaq <357229046@qq.com>
2025-08-14 20:24:29 +0800

    types: improve types

--- src/util/duration.ts
+++ src/util/duration.ts
@@ -1,4 +1,16 @@
-export function duration() {
+export interface DurationContext {
+  years(value: number): this
+  months(value: number): this
+  weeks(value: number): this
+  days(value: number): this
+  hours(value: number): this
+  minutes(value: number): this
+  seconds(value: number): this
+  milliseconds(value: number): this
+  valueOf(options?: { milliseconds: boolean }): number
+}
+
+export function duration(): DurationContext {
   const ctx = {
     value: 0,
     years,
@@ -12,42 +24,42 @@ export function duration() {
     valueOf,
   }
 
-  function years(value: number): typeof ctx {
+  function years(value: number) {
     ctx.value += value * 365 * 24 * 60 * 60 * 1000
     return ctx
   }
 
-  function months(value: number): typeof ctx {
+  function months(value: number) {
     ctx.value += value * 30 * 24 * 60 * 60 * 1000
     return ctx
   }
 
-  function weeks(value: number): typeof ctx {
+  function weeks(value: number) {
     ctx.value += value * 7 * 24 * 60 * 60 * 1000
     return ctx
   }
 
-  function days(value: number): typeof ctx {
+  function days(value: number) {
     ctx.value += value * 24 * 60 * 60 * 1000
     return ctx
   }
 
-  function hours(value: number): typeof ctx {
+  function hours(value: number) {
     ctx.value += value * 60 * 60 * 1000
     return ctx
   }
 
-  function minutes(value: number): typeof ctx {
+  function minutes(value: number) {
     ctx.value += value * 60 * 1000
     return ctx
   }
 
-  function seconds(value: number): typeof ctx {
+  function seconds(value: number) {
     ctx.value += value * 1000
     return ctx
   }
 
-  function milliseconds(value: number): typeof ctx {
+  function milliseconds(value: number) {
     ctx.value += value
     return ctx
   }
